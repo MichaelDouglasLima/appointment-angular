80fbd65417472d5ed5a0516c25b5389d28d1b651
MichaelDouglasLima <michael.ignis2022@gmail.com>
2026-05-17 11:03:31 -0300

    normalizando formulario de cliente

--- src/app/modules/maintenance/pages/client-form-page/client-form-page.component.ts
+++ src/app/modules/maintenance/pages/client-form-page/client-form-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Client } from 'src/app/core/models/client';
 import { ClientService } from 'src/app/core/services/client.service';
@@ -9,53 +10,67 @@ import { ClientService } from 'src/app/core/services/client.service';
   styleUrls: ['./client-form-page.component.css'],
 })
 export class ClientFormPageComponent {
-  formGroupClient: FormGroup;
-
-  client: Client = {} as Client;
+  clientForm: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
     private clientService: ClientService,
+    private location: Location,
   ) {
-    this.formGroupClient = this.formBuilder.group({
-      id: {
-        value: null,
-        disabled: true,
-      },
+    this.clientForm = this.formBuilder.group({
+      // id: {
+      //   value: null,
+      //   disabled: true,
+      // },
+      id: [''],
       name: ['', [Validators.required, Validators.minLength(3)]],
       phone: ['', [Validators.required]],
       dateOfBirth: ['', [Validators.required]],
     });
   }
 
-  get cfgName() {
-    return this.formGroupClient.get('name');
-  }
-
-  get cfgPhone() {
-    return this.formGroupClient.get('phone');
-  }
-
-  get cfgDateOfBirth() {
-    return this.formGroupClient.get('dateOfBirth');
-  }
-
-  save(ngFormClient: any) {
-    Object.assign(this.client, this.formGroupClient.value);
-
-    if (this.formGroupClient.valid) {
-      this.clientService.save(this.client).subscribe({
+  save(): void {
+    if (this.clientForm.valid) {
+      this.clientService.save(this.clientForm.value).subscribe({
         next: () => {
-          alert('Usuário cadastrado com sucesso!');
-          ngFormClient.resetForm();
-          // this.client = {} as Client;
-          //this.formGroupClient.reset();
-          // this.client = {} as Client;
+          this.location.back();
         },
         error: () => {
-          alert('Ocorreu um erro ao salvar o usuário!');
+          alert('Erro ao salvar o cliente!');
         },
       });
     }
+  }
+
+  // save(ngFormClient: any) {
+  //   Object.assign(this.client, this.clientForm.value);
+
+  //   if (this.clientForm.valid) {
+  //     this.clientService.save(this.client).subscribe({
+  //       next: () => {
+  //         alert('Usuário cadastrado com sucesso!');
+  //         ngFormClient.resetForm();
+  //       },
+  //       error: () => {
+  //         alert('Ocorreu um erro ao salvar o usuário!');
+  //       },
+  //     });
+  //   }
+  // }
+
+  cancel(): void {
+    this.location.back();
+  }
+
+  get cfName() {
+    return this.clientForm.get('name');
+  }
+
+  get cfPhone() {
+    return this.clientForm.get('phone');
+  }
+
+  get cfDateOfBirth() {
+    return this.clientForm.get('dateOfBirth');
   }
 }
