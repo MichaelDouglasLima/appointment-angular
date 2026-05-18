005b3143796f9ab5bda236ab91610c93e6ad4500
MichaelDouglasLima <michael.ignis2022@gmail.com>
2026-05-18 19:50:08 -0300

    ajustando carregamnento de areas

--- src/app/modules/schedule/components/form-create-appointment/form-create-appointment.component.ts
+++ src/app/modules/schedule/components/form-create-appointment/form-create-appointment.component.ts
@@ -1,35 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Area } from 'src/app/core/models/area';
-import { AreaService } from 'src/app/core/services/area.service';
-import { ToastService } from 'src/app/core/services/toast.service';
 
 @Component({
   selector: 'app-form-create-appointment',
   templateUrl: './form-create-appointment.component.html',
   styleUrls: ['./form-create-appointment.component.css'],
 })
-export class FormCreateAppointmentComponent implements OnInit {
+export class FormCreateAppointmentComponent {
+  @Input()
   areas: Area[] = [];
-
-  constructor(
-    private areaService: AreaService,
-    private toastService: ToastService,
-  ) {}
-
-  ngOnInit(): void {
-    this.loadAreas();
-  }
-
-  loadAreas() {
-    this.areaService.getAreas().subscribe({
-      next: (areas) => {
-        this.areas = areas;
-      },
-      error: () => {
-        this.toastService.show('Erro ao carregar as áreas!', {
-          classname: 'bg-danger text-light',
-        });
-      },
-    });
-  }
 }
--- src/app/modules/schedule/pages/create-appointment-page/create-appointment-page.component.ts
+++ src/app/modules/schedule/pages/create-appointment-page/create-appointment-page.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Area } from 'src/app/core/models/area';
+import { AreaService } from 'src/app/core/services/area.service';
+import { ToastService } from 'src/app/core/services/toast.service';
 
 @Component({
   selector: 'app-create-appointment-page',
   templateUrl: './create-appointment-page.component.html',
-  styleUrls: ['./create-appointment-page.component.css']
+  styleUrls: ['./create-appointment-page.component.css'],
 })
-export class CreateAppointmentPageComponent {
+export class CreateAppointmentPageComponent implements OnInit {
+  areas: Area[] = [];
 
+  constructor(
+    private areaService: AreaService,
+    private toastService: ToastService,
+  ) {}
+
+  ngOnInit(): void {
+    this.loadAreas();
+  }
+
+  loadAreas() {
+    this.areaService.getAreas().subscribe({
+      next: (areas) => {
+        this.areas = areas;
+      },
+      error: () => {
+        this.toastService.show('Erro ao carregar as áreas!', {
+          classname: 'bg-danger text-light',
+        });
+      },
+    });
+  }
 }
