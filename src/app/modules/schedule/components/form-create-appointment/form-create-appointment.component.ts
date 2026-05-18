b637d9289ba7afb8a3b06b03983f0c2b0149636c
MichaelDouglasLima <michael.ignis2022@gmail.com>
2026-05-18 19:20:30 -0300

    ajustando formulario de agendamento

---
+++ src/app/modules/schedule/components/form-create-appointment/form-create-appointment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-create-appointment',
+  templateUrl: './form-create-appointment.component.html',
+  styleUrls: ['./form-create-appointment.component.css']
+})
+export class FormCreateAppointmentComponent {
+
+}
--- src/app/modules/schedule/schedule.module.ts
+++ src/app/modules/schedule/schedule.module.ts
@@ -7,6 +7,7 @@ import { CreateAppointmentPageComponent } from './pages/create-appointment-page/
 import { CancelAppointmentPageComponent } from './pages/cancel-appointment-page/cancel-appointment-page.component';
 import { ClientHistoryPageComponent } from './pages/client-history-page/client-history-page.component';
 import { ProfessionalWorkdaysPageComponent } from './pages/professional-workdays-page/professional-workdays-page.component';
+import { FormCreateAppointmentComponent } from './components/form-create-appointment/form-create-appointment.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ProfessionalWorkdaysPageComponent } from './pages/professional-workdays
     CancelAppointmentPageComponent,
     ClientHistoryPageComponent,
     ProfessionalWorkdaysPageComponent,
+    FormCreateAppointmentComponent,
   ],
   imports: [CommonModule, ScheduleRoutingModule],
 })
