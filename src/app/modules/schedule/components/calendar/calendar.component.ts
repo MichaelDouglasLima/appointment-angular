927e1b2e83afacf7968096ad9b9103dd67e20b8b
MichaelDouglasLima <michael.ignis2022@gmail.com>
2026-05-20 19:30:16 -0300

    desafio calendario

--- src/app/modules/schedule/components/calendar/calendar.component.ts
+++ src/app/modules/schedule/components/calendar/calendar.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class CalendarComponent {
   calendarMonth: Date = new Date();
+
+  onNextMonth() {
+    this.calendarMonth = new Date(this.calendarMonth);
+    this.calendarMonth.setMonth(this.calendarMonth.getMonth() + 1);
+    this.calendarMonth.setDate(1);
+  }
+
+  onPreviousMonth() {
+    this.calendarMonth = new Date(this.calendarMonth);
+    if (this.calendarMonth > new Date()) {
+      this.calendarMonth.setMonth(this.calendarMonth.getMonth() - 1);
+      this.calendarMonth.setDate(1);
+    }
+  }
 }
