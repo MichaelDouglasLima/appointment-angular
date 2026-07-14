4eafc251ee5dc999478ed8d65246c0ecc8049a89
MichaelDouglasLima <michael.ignis2022@gmail.com>
2026-07-14 11:02:38 -0300

    :test_tube: tests: Create E2E Tests

---
+++ cypress/e2e/home-page.cy.ts
@@ -0,0 +1,19 @@
+describe('Home Page', () => {
+  it('should load home page', () => {
+    cy.visit('http://localhost:4200');
+  });
+
+  it('Home Page: Cadastros->Cliente Should have Title: "Cadastro de Clientes"', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('#navbarDropdownCadastros').click();
+    cy.get('#navItemCliente').click();
+    cy.contains('Cadastro de Clientes').should('be.visible');
+  });
+
+  it('Home Page: Cadastros->Agendar Should have Title: "Fazer um agendamento"', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('#navbarDropdownAgendamentos').click();
+    cy.get('#navItemAgendar').click();
+    cy.contains('Fazer um agendamento').should('be.visible');
+  });
+});
